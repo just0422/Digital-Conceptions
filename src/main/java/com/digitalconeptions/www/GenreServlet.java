a6a4f93cb24eaf7a26b21655463ecbfe874cf174
justin <jusr0422@hotmail.com>
2016-04-05 12:25:53 -0400

    Added Genre Support and finished HomePageServlet
    web.xml : added support for rerouting requests to genre servlet
    GenreServlet : added genre servlet to filter all requests by genere
    HomePageServlet : finished adding pulling comics from DB and adding them to attributes

---
+++ src/main/java/com/digitalconeptions/www/GenreServlet.java
@@ -0,0 +1,33 @@
+package com.digitalconeptions.www;
+
+import com.googlecode.objectify.ObjectifyService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by justin on 4/5/16.
+ */
+public class GenreServlet extends HttpServlet{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String genre = req.getParameter("genre");
+
+        if (genre == null)
+            genre = "action";
+
+        ServletContext sc = getServletContext();
+        RequestDispatcher rd = sc.getRequestDispatcher("/genres/"+genre);
+
+
+        List<ComicInfo> comics= ObjectifyService.ofy().load().type(ComicInfo.class).filter("genre", genre).list();
+        req.setAttribute("comics", comics);
+
+        rd.forward(req, resp);
+    }
+}
