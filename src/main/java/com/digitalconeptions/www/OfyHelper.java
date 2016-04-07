054b668bc100fc802327b00dc2b530030cf8533c
justin <jusr0422@hotmail.com>
2016-04-07 16:35:42 -0400

    Fixed Search
    Search Page Servlet : handles search requests
    web.xml : modified to handle search page servlet
    Fixed JSPs to make correct search requests
    
    Fixed a bug that threw an error because there was no registered comic info class

---
+++ src/main/java/com/digitalconeptions/www/OfyHelper.java
@@ -0,0 +1,21 @@
+package com.digitalconeptions.www;
+
+import com.googlecode.objectify.ObjectifyService;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by justin on 4/7/16.
+ */
+public class OfyHelper implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent event) {
+        ObjectifyService.register(ComicInfo.class);
+        ObjectifyService.register(UserInfo.class);
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+        // App Engine does not currently invoke this method.
+    }
+
+}
